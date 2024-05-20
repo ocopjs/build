e70dc8c30bfb55165c7a73fe01656c37984fe9d5
Trần Ngọc Huy <truongduchuy910@gmail.com>
2024-05-20 15:49:25 +0700

    new project examples

---
+++ apps/new-project/index.js
@@ -0,0 +1,21 @@
+const { Ocop } = require("@ocopjs/ocop");
+const { MongooseAdapter } = require("@ocopjs/adapter-mongoose");
+const { GraphQLApp } = require("@ocopjs/app-graphql");
+const { Text } = require("@ocopjs/fields");
+const { MongoId } = require("@ocopjs/fields-mongoid");
+
+const ocop = new Ocop({
+  adapter: new MongooseAdapter({ mongoUri: "mongodb://localhost/ocop" }),
+});
+
+ocop.createList("Todo", {
+  fields: {
+    id: { type: MongoId },
+    name: { type: Text },
+  },
+});
+
+module.exports = {
+  ocop,
+  apps: [new GraphQLApp()],
+};
